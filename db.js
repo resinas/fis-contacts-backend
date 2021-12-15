0cfd102290d2abef1c71810ea5f4b20443e37a65
Manuel Resinas <resinas@us.es>
2021-12-15 18:06:37 +0100

    Deployment in Okteto with secret

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const DB_URL = (process.env.MONGO_URL || 'mongodb://mongo/test');
 
 const dbConnect = function() {
+    console.log("Trying to connect to: " + DB_URL)
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
     return mongoose.connect(DB_URL, { useNewUrlParser: true });
